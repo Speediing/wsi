d4d2eee46a30f056bec8c273dd6b2a6c5361eabb
Jason Wiker <Jason.wiker@vercel.com>
2024-09-23 13:06:19 -0600

    .

---
+++ apps/web/src/app/api/revalidate/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from "next/server";
+import { revalidateTag } from "next/cache";
+
+export async function GET(request: NextRequest) {
+  try {
+    // Revalidate the "featured" tag
+    revalidateTag("featured");
+
+    // Return a JSON response indicating success
+    return NextResponse.json(
+      { revalidated: true, now: Date.now() },
+      { status: 200 }
+    );
+  } catch (error) {
+    // If an error occurs, return an error response
+    return NextResponse.json(
+      { revalidated: false, message: "Error revalidating", now: Date.now() },
+      { status: 500 }
+    );
+  }
+}
